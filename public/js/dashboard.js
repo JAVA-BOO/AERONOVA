58da5348757d774100f802371efa9450019d10d7
JAVA-BOO <harbayomiamos@gmail.com>
2026-03-23 11:21:00 +0100

    addedd a loading state for the dashboard

---
+++ public/js/dashboard.js
@@ -0,0 +1,54 @@
+console.log(firebase);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCutN4TH1SahRC4TZ8RcyqkvUSt0Ig95FQ",
+  authDomain: "aeronova-d3210.firebaseapp.com",
+  databaseURL: "https://aeronova-d3210-default-rtdb.firebaseio.com",
+  projectId: "aeronova-d3210",
+  storageBucket: "aeronova-d3210.firebasestorage.app",
+  messagingSenderId: "546554345178",
+  appId: "1:546554345178:web:7a325137f9bb71584f17e3",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+console.log(app);
+
+const auth = firebase.auth();
+console.log(auth);
+
+function checkUserAuth() {
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/v8/firebase.User
+      var uid = user.uid;
+      forUserName.innerHTML = user.displayName || "user";
+      console.log(user, "im here");
+    } else {
+      // User is signed out
+      // ...
+      console.log("im in the else block");
+      window.location.href = "./login.html";
+    }
+  });
+}
+
+checkUserAuth();
+
+function logOutUser() {
+  let canLogout = confirm("are you sure?");
+  if (canLogout) {
+    if (canLogout) {
+      auth
+        .signOut()
+        .then(() => {
+          window.location.href = "../login.html";
+        })
+        .catch((error) => {
+          console.log(error);
+          alert(error.message);
+        });
+    }
+  }
+}
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,5 +1,7 @@
 console.log(firebase);
 
+// Required for side-effects
+
 const firebaseConfig = {
   apiKey: "AIzaSyCutN4TH1SahRC4TZ8RcyqkvUSt0Ig95FQ",
   authDomain: "aeronova-d3210.firebaseapp.com",
@@ -12,25 +14,27 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
+var provider = new firebase.auth.GoogleAuthProvider();
 console.log(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = firebase.firestore();
 
 const auth = firebase.auth();
 console.log(auth);
 
 let myButton = document.getElementById("signUpBtn");
 
-console.log(termsCheck);
-
 function signUpUser() {
   let email = document.getElementById("email").value.trim();
-  let username = document.getElementById("name").value.trim();
+  let fullName = document.getElementById("name").value.trim();
   let password = document.getElementById("password").value.trim();
   let confirm = document.getElementById("confirm").value.trim();
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   let termsCheck = document.getElementById("terms").checked;
 
-  if (!email || !username || !password || !confirm) {
+  if (!email || !fullName || !password || !confirm) {
     alert("all fields are mandatory");
   } else if (!emailPattern.test(email)) {
     alert("Please enter a valid email address.");
@@ -41,16 +45,34 @@ function signUpUser() {
   } else if (!termsCheck) {
     alert("You must accept the Terms of Service and Privacy Policy.");
   } else {
-    // showLoadingandDisable("loading..", true);
+    showLoadingandDisable("loading..", true);
+    // showLoadingandDisable("loading...", true);
+
     auth
       .createUserWithEmailAndPassword(email, password)
       .then((userCredential) => {
         // Signed in
-        var user = userCredential.user;
-        alert("sign up successful");
-        window.location.href = "./login.html";
-        // ...
+        const user = userCredential.user;
+
+        return user.updateProfile({ displayName: fullName }).then(() => user);
       })
+      .then((user) => {
+        return db.collection("users").doc(user.uid).set({
+          uid: user.uid,
+          email: user.email,
+          name: user.displayName,
+          createdAt: new Date(),
+          role: "user",
+          status: "active",
+        });
+      })
+      .then(() => {
+        alert("sign up successful");
+        // alert(user.displayName)
+        window.location.href = "./login.html";
+        showLoadingandDisable("Create Account", false);
+      })
+
       .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -59,4 +81,35 @@ function signUpUser() {
         // ..
       });
   }
+}
+
+function showLoadingandDisable(text, isloading) {
+  myButton.textContent = text;
+  myButton.disabled = isloading;
+}
+
+function signInGoogle() {
+  auth
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // IdP data available in result.additionalUserInfo.profile.
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
 }
